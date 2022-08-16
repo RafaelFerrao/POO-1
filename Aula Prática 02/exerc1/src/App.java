00771f7e5a00838888a9c2d25f95adcc8dbd5740
Rafael Ferrao <rafa5ferrao@gmail.com>
2022-08-16 19:18:05 -0300

    changes

--- Aula Prática 02/exerc1/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Pessoa p1 = new Pessoa("Rafael", 6, 10, 2001, 180);
-
-
-        System.out.println(p1.toString());
-    }
-}
--- Exercicios_01/Exercicios_01/src/Exercicio03.java
+++
@@ -1,9 +0,0 @@
-public class Exercicio03 {
-    public static void main(String[] args) {
-        double n1 = 7.7, n2 = 9.5, mediaArit, mediaPond;
-        mediaArit = (n1 + n2) / 2;
-        mediaPond = (n1 * 2 + n2 * 3) / 5;
-        System.out.println("Media Aritmetica: " + mediaArit);
-        System.out.println("Media Ponderada: " + mediaPond);
-    };
-}
